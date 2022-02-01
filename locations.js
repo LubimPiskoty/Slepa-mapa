d1b20090a242505bad525d81d417727c83e47005
LubimPiskoty <54437068+LubimPiskoty@users.noreply.github.com>
2022-02-01 19:41:58 +0100

    ✔ Marker system added

---
+++ locations.js
@@ -0,0 +1,16 @@
+var PLACES = {
+	"Očová": { 
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [19.2933708, 48.6088194]
+		}},
+	"Handlova": 
+		{ 
+			"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [18.6712528, 48.6953449]
+		}}
+};
+
--- main.js
+++ main.js
@@ -1,23 +1,53 @@
-// var map = L.map('map').setView([51.505, -0.09], 13);
-
-L.mapbox.accessToken = 'pk.eyJ1IjoibW8wbmRhcyIsImEiOiJja3o0NXdsZzMwMWd0MnZxdzgzdjg5bWRvIn0.w7x7bdi9iEu9q9r-rTMZzA';
+L.mapbox.accessToken =
+	"pk.eyJ1IjoibW8wbmRhcyIsImEiOiJja3o0NXdsZzMwMWd0MnZxdzgzdjg5bWRvIn0.w7x7bdi9iEu9q9r-rTMZzA";
 // Create a map in the div #map
-var map = L.mapbox.map('map')
-    .setView([51.505, -0.09], 13)
-    .addLayer(L.mapbox.styleLayer('mapbox://styles/mo0ndas/ckz46276w000914p8b7dkhhhl'));
+var map = L.mapbox
+	.map("map")
+	.setView([51.505, -0.09], 4)
+	.addLayer(
+		L.mapbox.styleLayer("mapbox://styles/mo0ndas/ckz4el2ft001q15sxlyzqn2tt")
+	);
+// .addLayer(L.mapbox.styleLayer('mapbox://styles/mo0ndas/ckz46276w000914p8b7dkhhhl'));
 
-    
-    var polygon = L.polygon([
-    [51.509, -0.08],
-    [51.503, -0.06],
-    [51.51, -0.047]
-]).addTo(map);
+map.options.minZoom = 4;
+map.options.maxZoom = 6;
 
-var circle = L.circle([51.508, -0.11], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(map);
+var popup = L.popup();
 
-var marker = L.marker([51.5, -0.09]).addTo(map);
+function onMapClick(e) {
+	let pos = e.latlng
+		.toString()
+		.substring(7)
+		.replace("(", "")
+		.replace(")", "")
+		.split(", ");
+	popup
+		.setLatLng(e.latlng)
+		.setContent(pos[1] + ", " + pos[0])
+		.openOn(map);
+}
+
+map.on("click", onMapClick);
+
+var geojsonMarkerOptions = {
+	radius: 8,
+	fillColor: "#ff7800",
+	color: "#000",
+	weight: 1,
+	opacity: 1,
+	fillOpacity: 0.8,
+};
+
+let geoLayer = L.geoJSON("", {
+		pointToLayer: function (feature, latlng) {
+			return L.circleMarker(latlng, geojsonMarkerOptions);
+		},
+	}).addTo(map);
+
+let newPoint = (feature) => {
+	geoLayer.addData(feature);
+};
+
+let clearPoints = () => {
+	geoLayer.clearLayers();
+};
